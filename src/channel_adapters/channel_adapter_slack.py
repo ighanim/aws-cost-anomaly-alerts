ae8161e90e09dd0fdf3151d4fdb672a92f55ae75
Ghanim <ghanimig@3c22fb428b64.ant.amazon.com>
2022-02-23 21:16:59 +1100

    Add License Header

--- src/channel_adapters/channel_adapter_slack.py
+++ src/channel_adapters/channel_adapter_slack.py
@@ -1,3 +1,5 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
 import json
 from slack_sdk.webhook import WebhookClient
 from slack.models import Block, Text, Field
