ea614111ad58a465dce04e0c5d61a4fb0141a9a6
Jacky Wong <jackylw@amazon.com>
2023-08-07 23:55:18 +0800

    added lambda deployment version

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,15 +1,15 @@
 resource "aws_lambda_function" "CostAnomalyAlertsLambda" {
+  description = "Lambda Function to Send Cost Anomaly Alerts to different channels"
   filename      = "${path.module}/lambda-package-v1.0.0.zip"
   function_name = "CostAnomalyAlertsLambda"
   role          = aws_iam_role.CostAnomalyAlertsLambdaRole.arn
   handler       = "lambda_function.lambda_handler"
-
-
   runtime     = "python3.7"
   layers      = [lookup(var.app_config_layer_arn, data.aws_region.current.name, "")]
   timeout     = 15
-  description = "Lambda Function to Send Cost Anomaly Alerts to different channels"
-
+  
+  publish = true
+  
   tags = {
     Project = "CostAnomalyAlerts"
   }
@@ -18,7 +18,7 @@ resource "aws_lambda_function" "CostAnomalyAlertsLambda" {
 resource "aws_lambda_alias" "CostAnomalyAlertsLambdaAliasProd" {
   name             = "prod"
   function_name    = aws_lambda_function.CostAnomalyAlertsLambda.arn
-  function_version = "$LATEST" 
+  function_version = "1" 
 
 }
 
